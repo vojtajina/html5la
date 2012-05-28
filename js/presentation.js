892dc1a77b27b269c92b8785681aa00422900e34
Vojta Jina <vojta.jina@gmail.com>
2012-05-27 19:20:34 -0700

    Fix navigation to not overflow and rewrite invalid paths
    $location.path() is source of the truth.

--- js/presentation.js
+++ js/presentation.js
@@ -1,47 +1,59 @@
 var presentation = angular.module('presentation', []);
 
 presentation.controller('PresentationController', function($scope, $location, keyboard) {
-  var scope = $scope;
   var RIGHT_ARROW = 39;
   var LEFT_ARROW = 37;
   var PAGE_UP = 33;
   var PAGE_DOWN = 34;
 
+  /*
+    activeSlide = zero based number of current slide
+    $location.path contains 1 based number of current slide
+  */
+
   keyboard.on([RIGHT_ARROW, PAGE_DOWN], function() {
-    scope.activeSlide++;
+    if ($scope.activeSlide < $scope.totalSlides - 1) {
+      $location.path('/slides/' + ($scope.activeSlide + 2));
+    } else if ($scope.activeSlide === $scope.totalSlides - 1) {
+      $location.path('/slides/end');
+    }
   });
 
   keyboard.on([LEFT_ARROW, PAGE_UP], function() {
-    scope.activeSlide--;
-  });
-
-  scope.$watch('activeSlide', function(value) {
-    if (value == -1) {
-      $location.url('');
-    } else if (value > -1) {
-      $location.url('/slides/' + (value + 1));
+    if (-1 < $scope.activeSlide) {
+      $location.path('/slides/' + ($scope.activeSlide || ''));
     }
   });
 
-  scope.$watch(function() { return $location.url(); }, function(value) {
-    var match = /\/slides\/(\d+)/.exec(value);
-    if (match) {
-      scope.activeSlide = parseInt(match[1], 10) - 1;
-    } else if (value == '/slides/end') {
-      scope.activeSlide = scope.totalSlides;
+  $scope.$watch(function() { return $location.path(); }, function(value) {
+    var match = /\/slides\/(.*)/.exec(value);
+
+    if (!match) {
+      $location.path('/slides/');
+    } else if (match[1] === '') {
+      $scope.activeSlide = -1;
+    } else if (match[1] === 'end') {
+      $scope.activeSlide = $scope.totalSlides;
+    } else {
+      var i = parseInt(match[1], 10);
+      if (1 <= i && i <= $scope.totalSlides) {
+        $scope.activeSlide = i - 1;
+      } else {
+        $location.path('/slides/');
+      }
     }
   });
 
-  scope.isInsideDeck = function() {
+  $scope.isInsideDeck = function() {
     return !this.isBefore() && !this.isAfter();
   };
 
-  scope.isBefore = function() {
-    return scope.activeSlide < 0;
+  $scope.isBefore = function() {
+    return $scope.activeSlide === 0;
   };
 
-  scope.isAfter = function() {
-    return scope.activeSlide >= scope.totalSlides;
+  $scope.isAfter = function() {
+    return $scope.activeSlide >= $scope.totalSlides;
   };
 });
 
@@ -64,23 +76,10 @@ presentation.directive('deck', function() {
     restrict: 'E',
     link: function(scope, element, attr) {
       var slides = element.find('slide');
-      var name = element.attr('current');
-      var total = element.attr('total');
 
-      function restack() {
-        slides.each(function(i, slide) {
-          slide.style.zIndex = 'auto';
-          if ($(slide).hasClass('next')) {
-            slide.style.zIndex = -i;
-          }
-        });
-      }
+      scope.$eval(attr.total + ' = ' + slides.length);
 
-      restack();
-
-      scope.$eval(total + ' = ' + slides.length);
-
-      scope.$watch(name, function(value) {
+      scope.$watch(attr.current, function(value) {
         slides.each(function(i, slide) {
           $(slide).removeClass('previous current next');
           if (i < value) {
@@ -92,13 +91,13 @@ presentation.directive('deck', function() {
           }
         });
 
-        if (value < -1 || isNaN(value)) {
-          scope.$eval(name + ' = -1');
-        } else if (value > slides.length) {
-          scope.$eval(name + ' = ' + slides.length);
-        }
-
-        restack();
+        // restack
+        slides.each(function(i, slide) {
+          slide.style.zIndex = 'auto';
+          if ($(slide).hasClass('next')) {
+            slide.style.zIndex = -i;
+          }
+        });
       });
     }
   };
